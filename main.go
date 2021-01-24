6c1843605a7c207aa59c765fadf119c78b9e115b
Melih Sivri <mlheymen.ms@gmail.com>
2021-01-24 14:43:33 +0300

    Update main.go

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func SendMP3(){
 func downloadMP3(url string) error{
 	command1 := exec.Command("cd","bin/utils/")
 	command2 := exec.Command("youtube-dl","-x","--audio-format","mp3","-o","/file.mp4",url)
-	log.Println(exec.Command("ls").Run())
+	log.Println(exec.Command("ls").Output())
 	log.Println(command1.Run())
 	_,err := command2.Output()
 	return err
